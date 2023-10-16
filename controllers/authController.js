d12955bcfee62350cc97ed03e653622d3b9f8e5f
Rahul Vijay Singh <singhrahulsingh640@gmail.com>
2023-10-16 19:44:25 +0530

    Registeration backend api is working fine with thunker client

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,54 @@
-export const registerController = () => { }; 
+import { hashPassword } from "../helpers/authhelper.js";
+import userModel from "../models/userModel.js";
+
+export const registerController = async (req, res) => {
+    try {
+        const { name, email, password, phone, address } = req.body
+        // validation
+        if (!name) {
+            return res.send({ error: "Name is required" })
+        }
+        if (!email) {
+            return res.send({ error: "Nmae is Required" })
+        }
+        if (!password) {
+            return res.send({ error: "Nmae is Required" })
+        }
+        if (!phone) {
+            return res.send({ error: "Nmae is Required" })
+        }
+        if (!address) {
+            return res.send({ error: "Nmae is Required" })
+        }
+
+        // check user
+        const existinguser = await userModel.findOne({ email: email })
+
+        // existing user
+        if (existinguser) {
+            return res.status(200).send({
+                success: true,
+                message: "Already Registerd please login",
+            })
+        }
+
+        // regiter user
+        const hashedPassword = await hashPassword(password)
+        // save
+        const user = await new userModel({ name, email, phone, address, password: hashedPassword }).save()
+
+        res.status(201).send({
+            success: true,
+            message: "User Registerd successfully",
+            user,
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in Registration",
+            error
+        })
+    }
+}; 
--- models/userModel.js
+++ models/userModel.js
@@ -2,29 +2,29 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: {
-        type: string,
+        type: String,
         required: true,
         trim: true,
     },
     email: {
-        type: string,
+        type: String,
         required: true,
         unique: true
     },
     password: {
-        type: string,
+        type: String,
         required: true,
     },
     phone: {
-        type: string,
+        type: String,
         required: true
     },
     address: {
-        type: string,
+        type: String,
         required: true,
     },
     role: {
-        type: integer,
+        type: Number,
         default: 0
     }
 
