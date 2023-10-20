38bb00f8f2f1a851b2033427aa4118e0f329abe6
Rahul Vijay Singh <singhrahulsingh640@gmail.com>
2023-10-21 02:14:23 +0530

    worked in login routing login api is working

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import { registerController } from '../controllers/authController.js';
+import { registerController, loginController } from '../controllers/authController.js';
 
 const router = express.Router();
 
+// regestration route
 router.post('/register', registerController);
+
+// login route 
+router.post('/login', loginController);
 
 export default router
