b78daaa119023c81e31a408c90ff86d22a6efedb
Rahul Vijay Singh <singhrahulsingh640@gmail.com>
2024-01-12 06:23:07 +0530

    crud of  create product

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import morgan from 'morgan'
 import authRoutes from './routes/authRoutes.js'
 import cors from 'cors'
 import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from './routes/productRoutes.js'
 
 
 
@@ -26,6 +27,7 @@ app.use(morgan('dev'))
 // routes
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/category', categoryRoutes);
+app.use('/api/v1/product', productRoutes);
 
 // rest api
 app.get('/', (req, res) => {
