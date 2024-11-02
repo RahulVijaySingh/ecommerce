80575e9d1dbf88f7f025a94a152266e0699e7ac6
Rahul Vijay Singh <singhrahulsingh640@gmail.com>
2024-11-03 01:42:43 +0530

    user oder list and also order list for admin is done

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ import ProductDetails from "./pages/ProductDetails.js";
 import Categories from "./pages/Categories.js";
 import CategoryProduct from "./pages/CategoryProduct.js";
 import CartPage from "./pages/CartPage.js";
-// import UpdateProduct from "./pages/Admin/UpdateProduct";
+import AdminOrders from "./pages/Admin/AdminOrders.js";
 
 function App() {
   return (
@@ -51,6 +51,7 @@ function App() {
           <Route path="admin/product/:slug" element={<UpdateProduct />} />
           <Route path="admin/products" element={<Products />} />
           <Route path="admin/users" element={<Users />} />
+          <Route path="admin/orders" element={<AdminOrders />} />
         </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,9 @@ import {
   testController,
   forgotPasswordController,
   updateProfileController,
+  getAllOrdersController,
+  getOrdersController,
+  orderStatusController,
 } from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import { updateProductController } from "../controllers/productController.js";
@@ -33,5 +36,19 @@ router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
 });
 
 router.put("/profile", requireSignIn, updateProfileController);
+
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+router.put(
+  "/order-status/:orderId",
+  requireSignIn,
+  isAdmin,
+  orderStatusController
+);
 
 export default router;
